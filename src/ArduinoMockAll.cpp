4d2c8589c89416903da466d38a3e330a9732dcad
Jacob Rosenthal <jakerosenthal@gmail.com>
2015-02-11 23:34:35 -0700

    rename arduino sources to cpp to match arduino imports

---
+++ src/ArduinoMockAll.cpp
@@ -0,0 +1,10 @@
+// Copyright 2015 http://switchdevice.com
+
+#include "Arduino.cpp"
+#include "EEPROM.cpp"
+#include "Serial.cpp"
+#include "serialHelper.cpp"
+#include "Spark.cpp"
+#include "WiFi.cpp"
+#include "Wire.cpp"
+
